f02ca5a5b34854f2e121975c272957a059afa57e
jonathanguyen21 <119833065+jonathanguyen21@users.noreply.github.com>
2023-08-09 23:09:27 -0700

    saving

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h3 className="font-medium">test</h3>
+      <h3 className="text-red-600 font-bold">tests</h3>
+      <h3 className="text"></h3>
     </>
   );
 }
