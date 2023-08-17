ebdc89144d308d99aa29bf0320df1278b4cd45e0
jonathanguyen21 <119833065+jonathanguyen21@users.noreply.github.com>
2023-08-16 18:30:35 -0700

    Fetches requests from OpenAI Successfully

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import {
 import { useState } from "react";
 
 function App() {
+  const [typing, setTyping] = useState(false);
+
   const [messages, setMessages] = useState([
     {
       message: "Hello, I am ChatGPT!",
@@ -25,20 +27,72 @@ function App() {
       direction: "outgoing",
     };
 
-    const newMessages = [...messages, newMessage];
+    const newMessages = [...messages, newMessage]; // all old messages + new messages
 
     // update our messages state
     setMessages(newMessages);
 
+    // set a typing indicator (GPT is typing)
+    setTyping(true);
+
     // process message to chatGPT (send to server and see response)
+    await processMessageToChatGPT(newMessages);
   };
+
+  async function processMessageToChatGPT(chatMessages) {
+    // chatMessages { sender: "user" or "ChatGPT", message: "Message Content"}
+    // apiMessages { role: "user" or "assistant, content: "Message content" }
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return {
+        role: role,
+        content: messageObject.message,
+      };
+    });
+
+    const systemMessage = {
+      role: "system",
+      content: "Explain all concepts like I am 10 years old.",
+    };
+
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + process.env.REACT_APP_OPENAI_API,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        console.log(data.choices[0].message.content);
+      });
+  }
 
   return (
     <>
       <div className="relative h-[800px] w-[700px]">
         <MainContainer>
           <ChatContainer>
-            <MessageList>
+            <MessageList
+              typingIndicator={
+                typing ? <TypingIndicator content="ChatGPT is typing" /> : null
+              }
+            >
               {messages.map((message, i) => {
                 return <Message key={i} model={message} />;
               })}
