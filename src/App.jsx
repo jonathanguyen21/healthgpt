b9730602dd2a563c7a2750cf1a39a46f035b48de
jonathanguyen21 <119833065+jonathanguyen21@users.noreply.github.com>
2023-08-16 17:47:33 -0700

    Created chatbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
 import "./App.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+import { useState } from "react";
 
 function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am ChatGPT!",
+      sender: "ChatGPT",
+    },
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing",
+    };
+
+    const newMessages = [...messages, newMessage];
+
+    // update our messages state
+    setMessages(newMessages);
+
+    // process message to chatGPT (send to server and see response)
+  };
+
   return (
     <>
-      <h3 className="text-red-600 font-bold">tests</h3>
-      <h3 className="text"></h3>
+      <div className="relative h-[800px] w-[700px]">
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />;
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </>
   );
 }
